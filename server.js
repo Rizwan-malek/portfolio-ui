65c588698fff5cada379c42edb9adc1cd1c18bef
Rizwan <125858315+Rizwan-malek@users.noreply.github.com>
2025-11-12 10:42:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,4 +24,5 @@ app.get("/", (req, res) => {
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`✅ Server started on http://localhost:${PORT}
+  console.log(`✅ Server started on http://localhost:${PORT}`);
+});
